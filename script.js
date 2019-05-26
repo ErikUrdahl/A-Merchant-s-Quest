5feb824eb632d9a313a985a147ebb604d33cdab8
ErikUrdahl <35401873+ErikUrdahl@users.noreply.github.com>
2019-05-26 12:22:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
+function clicked() {
+ var pChanged= document.getElementById("clicked");
+ pChanged.button.clicked= "#A81872";
+}
